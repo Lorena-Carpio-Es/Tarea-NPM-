d3462cbf3726b13fdf7e0c4b39200eb95845af43
Lorena-Carpio-Es <karylore1@gmail.com>
2025-03-04 20:51:24 -0400

    Modificar el presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,9 @@
-import sumar from "./sumador";
+import {sumar,multiplicar} from "./sumador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -12,4 +13,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarButton.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevenir el comportamiento predeterminado
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
